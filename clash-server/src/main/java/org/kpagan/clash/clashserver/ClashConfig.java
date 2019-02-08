dabb7c2f4ea180184ffd13deb6ffa7962c488628
Kostas Paganelis <konstantinos.paganelis@trasysgroup.com>
2019-02-08 12:26:32 +0200

    conditional resttemplate creation based on existing env variable fixie_url or not

--- clash-server/src/main/java/org/kpagan/clash/clashserver/ClashConfig.java
+++ clash-server/src/main/java/org/kpagan/clash/clashserver/ClashConfig.java
@@ -7,6 +7,7 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,30 +18,36 @@ import org.springframework.web.client.RestTemplate;
 @EnableCaching
 public class ClashConfig {
 
+	@Value("${FIXIE_URL:#{null}}")
+	private String fixieUrl;
+	
 	@Bean
 	public RestTemplate createRestTemplate() throws Exception {
-		String fixieUrl = System.getenv("FIXIE_URL");
+		if (fixieUrl != null) {
+	        String[] fixieValues = fixieUrl.split("[/(:\\/@)/]+");
+	        String fixieUser = fixieValues[1];
+	        String fixiePassword = fixieValues[2];
+	        String fixieHost = fixieValues[3];
+	        int fixiePort = Integer.parseInt(fixieValues[4]);
+	        
+	        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+	        credsProvider.setCredentials( 
+	                new AuthScope(fixieHost, fixiePort), 
+	                new UsernamePasswordCredentials(fixieUser, fixiePassword));
 
-        String[] fixieValues = fixieUrl.split("[/(:\\/@)/]+");
-        String fixieUser = fixieValues[1];
-        String fixiePassword = fixieValues[2];
-        String fixieHost = fixieValues[3];
-        int fixiePort = Integer.parseInt(fixieValues[4]);
-        
-        CredentialsProvider credsProvider = new BasicCredentialsProvider();
-        credsProvider.setCredentials( 
-                new AuthScope(fixieHost, fixiePort), 
-                new UsernamePasswordCredentials(fixieUser, fixiePassword));
+	        HttpHost myProxy = new HttpHost(fixieHost, fixiePort);
+	        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
 
-        HttpHost myProxy = new HttpHost(fixieHost, fixiePort);
-        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+	        clientBuilder.setProxy(myProxy).setDefaultCredentialsProvider(credsProvider).disableCookieManagement();
 
-        clientBuilder.setProxy(myProxy).setDefaultCredentialsProvider(credsProvider).disableCookieManagement();
+	        HttpClient httpClient = clientBuilder.build();
+	        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+	        factory.setHttpClient(httpClient);
 
-        HttpClient httpClient = clientBuilder.build();
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setHttpClient(httpClient);
-
-        return new RestTemplate(factory);
+	        return new RestTemplate(factory);
+		} else {
+			return new RestTemplate();
+		}
     }
+	
 }
