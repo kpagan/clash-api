45acd7219ba12c287ad318cff9966ded0bb2fb8d
Kostas Paganelis <konstantinos.paganelis@trasysgroup.com>
2019-04-17 18:23:59 +0300

    added persistence layer with H2

---
+++ clash-server/src/main/java/org/kpagan/clash/clashserver/domain/ClanMember.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.kpagan.clash.clashserver.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NaturalId;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author paganelis
+ *
+ */
+@Entity
+@Table(name = "clan_member")
+@Getter @Setter
+public class ClanMember {
+
+	@Id
+	@NaturalId
+	private String tag;
+
+	private String name;
+
+	@Column(name = "member_since")
+	private LocalDate memberSince;
+
+	@Column(name = "left_clan")
+	private LocalDate leftClan;
+
+	@Column(name = "donated_from_join")
+	private Integer donatedFromJoinDay;
+	
+	@Column(name = "received_from_join")
+	private Integer receivedFromJoinDay;
+
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ClanMember)) return false;
+        ClanMember member = (ClanMember) o;
+        return Objects.equals(getTag(), member.getTag());
+    }
+ 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTag());
+    }
+	
+}
